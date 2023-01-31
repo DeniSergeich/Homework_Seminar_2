73ed6201247bf2fd55e40178f6ed66608f09fec8
DenniSergeich <damned_wolf@mail.ru>
2023-01-31 17:35:39 +0300

    Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int lenght = number.Length;
+if (lenght < 3)
+{
+    Console.WriteLine("Третьей цифры нет!");
+    return;
+}
+char num = number[2];
+Console.WriteLine($"Третья цифра: {num}");
