426b4ce9425a8c29afe7736531ea1ca69ba3c449
DenniSergeich <damned_wolf@mail.ru>
2023-01-31 17:36:43 +0300

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите день недели от 1 до 7: ");
+int day = int.Parse(Console.ReadLine());
+if (day < 1 || day > 7)
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
