96010ed43d0f594a6c59dd1b9997bdaef32a5400
DenniSergeich <damned_wolf@mail.ru>
2023-01-31 17:34:01 +0300

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите трехзачное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+Console.WriteLine(number % 100 / 10);
